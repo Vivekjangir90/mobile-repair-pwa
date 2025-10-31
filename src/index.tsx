a5ace71fbf187769935b875ee8878e3bc80a9dc2
vivekjangir90 <vivekjangir90@gmail.com>
2025-10-31 23:45:38 +0530

    Initial upload from Termux

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './styles/index.css'; // Tailwind CSS ko import karein
 import AppRoutes from './App.tsx'; // App.tsx file se import karein
 
 // Yeh Service Worker registration PWA ke liye zaroori hai
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+//import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // serviceWorkerRegistration file bhi aapko project mein include karni hogi
 
@@ -19,4 +19,4 @@ root.render(
 );
 
 // Service worker ko register karein
-serviceWorkerRegistration.register(); 
+//serviceWorkerRegistration.register(); 
